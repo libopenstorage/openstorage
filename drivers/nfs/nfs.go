91b367a28122b0bdebf4f11ec8465c5747d1e8ae
Gou Rao <gou@portworx.com>
2015-09-15 22:06:19 +0000

    Emulate snaps over NFS

--- drivers/nfs/nfs.go
+++ drivers/nfs/nfs.go
@@ -3,6 +3,7 @@ package nfs
 import (
 	"errors"
 	"fmt"
+	"io"
 	"os"
 	"path"
 	"strings"
@@ -29,9 +30,77 @@ const (
 // Implements the open storage volume interface.
 type driver struct {
 	*volume.DefaultEnumerator
-	*volume.SnapshotNotSupported
 	nfsServer string
 	nfsPath   string
+}
+
+func copyFile(source string, dest string) (err error) {
+	sourcefile, err := os.Open(source)
+	if err != nil {
+		return err
+	}
+
+	defer sourcefile.Close()
+
+	destfile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer destfile.Close()
+
+	_, err = io.Copy(destfile, sourcefile)
+	if err == nil {
+		sourceinfo, err := os.Stat(source)
+		if err != nil {
+			err = os.Chmod(dest, sourceinfo.Mode())
+		}
+
+	}
+
+	return
+}
+
+func copyDir(source string, dest string) (err error) {
+	// get properties of source dir
+	sourceinfo, err := os.Stat(source)
+	if err != nil {
+		return err
+	}
+
+	// create dest dir
+
+	err = os.MkdirAll(dest, sourceinfo.Mode())
+	if err != nil {
+		return err
+	}
+
+	directory, _ := os.Open(source)
+
+	objects, err := directory.Readdir(-1)
+
+	for _, obj := range objects {
+
+		sourcefilepointer := source + "/" + obj.Name()
+
+		destinationfilepointer := dest + "/" + obj.Name()
+
+		if obj.IsDir() {
+			// create sub-directories - recursively
+			err = copyDir(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		} else {
+			// perform copy
+			err = copyFile(sourcefilepointer, destinationfilepointer)
+			if err != nil {
+				fmt.Println(err)
+			}
+		}
+
+	}
+	return
 }
 
 func Init(params volume.DriverParams) (volume.VolumeDriver, error) {
@@ -189,6 +258,35 @@ func (d *driver) Unmount(volumeID api.VolumeID, mountpath string) error {
 	v.AttachPath = ""
 	err = d.UpdateVol(v)
 	return err
+}
+
+func (d *driver) Snapshot(volumeID api.VolumeID, readonly bool, locator api.VolumeLocator) (api.VolumeID, error) {
+	volIDs := make([]api.VolumeID, 1)
+	volIDs[0] = volumeID
+	vols, err := d.Inspect(volIDs)
+	if err != nil {
+		return api.BadVolumeID, nil
+	}
+
+	newVolumeID, err := d.Create(locator, nil, vols[0].Spec)
+	if err != nil {
+		return api.BadVolumeID, nil
+	}
+
+	// NFS does not support snapshots, so just copy the files.
+	err = copyDir(nfsMountPath+string(volumeID), nfsMountPath+string(newVolumeID))
+	if err != nil {
+		d.Delete(newVolumeID)
+		return api.BadVolumeID, nil
+	}
+
+	err = copyFile(nfsMountPath+string(volumeID)+nfsBlockFile, nfsMountPath+string(newVolumeID)+nfsBlockFile)
+	if err != nil {
+		d.Delete(newVolumeID)
+		return api.BadVolumeID, nil
+	}
+
+	return newVolumeID, nil
 }
 
 func (d *driver) Attach(volumeID api.VolumeID) (string, error) {
