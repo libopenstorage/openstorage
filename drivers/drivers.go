fa16e8fd6a70b0d2d6d3d940ed55a3fc3f8de0f1
Peter Edge <peter.edge@gmail.com>
2015-10-09 04:44:47 +0000

    move osd.go to cmd/osd/main.go, move drivers.go to drivers/drivers.go

--- drivers/drivers.go
+++ drivers/drivers.go
@@ -1,32 +1,33 @@
-package main
+package drivers
 
 import (
 	"github.com/libopenstorage/openstorage/drivers/aws"
 	"github.com/libopenstorage/openstorage/drivers/btrfs"
-	"github.com/libopenstorage/openstorage/drivers/vfs"
 	"github.com/libopenstorage/openstorage/drivers/nfs"
 	"github.com/libopenstorage/openstorage/drivers/pwx"
+	"github.com/libopenstorage/openstorage/drivers/vfs"
 	"github.com/libopenstorage/openstorage/volume"
 )
 
 // Driver is the description of a supported OST driver. New Drivers are added to
 // the drivers array
 type Driver struct {
-	driverType volume.DriverType
-	name       string
+	DriverType volume.DriverType
+	Name       string
 }
 
 var (
-	drivers = []Driver{
+	// AllDrivers is a slice of all existing known Drivers.
+	AllDrivers = []Driver{
 		// AWS driver provisions storage from EBS.
-		{driverType: aws.Type, name: aws.Name},
+		{DriverType: aws.Type, Name: aws.Name},
 		// NFS driver provisions storage from an NFS server.
-		{driverType: nfs.Type, name: nfs.Name},
+		{DriverType: nfs.Type, Name: nfs.Name},
 		// BTRFS driver provisions storage from local btrfs.
-		{driverType: btrfs.Type, name: btrfs.Name},
+		{DriverType: btrfs.Type, Name: btrfs.Name},
 		// PWX driver provisions storage from PWX cluster.
-		{driverType: pwx.Type, name: pwx.Name},
+		{DriverType: pwx.Type, Name: pwx.Name},
 		// VFS driver provisions storage from local filesystem
-		{driverType: vfs.Type, name: vfs.Name},
+		{DriverType: vfs.Type, Name: vfs.Name},
 	}
 )
