693c55d9c0dbfb2343489c42ca1a96236318b245
Peter Edge <peter.edge@gmail.com>
2016-02-08 23:32:54 +0100

    clean up api/client/client.go
    Signed-off-by: Peter Edge <peter.edge@gmail.com>

--- api/client/client.go
+++ api/client/client.go
@@ -14,6 +14,41 @@ import (
 	"github.com/libopenstorage/openstorage/volume"
 )
 
+var (
+	httpCache = make(map[string]*http.Client)
+	cacheLock sync.Mutex
+)
+
+// NewClient returns a new REST client for specified server.
+func NewClient(host string, version string) (*Client, error) {
+	baseURL, err := url.Parse(host)
+	if err != nil {
+		return nil, err
+	}
+	if baseURL.Path == "" {
+		baseURL.Path = "/"
+	}
+	unix2HTTP(baseURL)
+	c := &Client{
+		base:       baseURL,
+		version:    version,
+		httpClient: getHttpClient(host),
+	}
+	return c, nil
+}
+
+// NewClusterClient returns a new REST client for cluster management.
+func NewClusterClient() (*Client, error) {
+	sockPath := "unix://" + config.ClusterAPIBase + "osd.sock"
+	return NewClient(sockPath, config.Version)
+}
+
+// NewDriver returns a new REST client for specified driver.
+func NewDriverClient(driverName string) (*Client, error) {
+	sockPath := "unix://" + config.DriverAPIBase + driverName + ".sock"
+	return NewClient(sockPath, config.Version)
+}
+
 // Client is an HTTP REST wrapper. Use one of Get/Porst/Put/Delete to get a request
 // object.
 type Client struct {
@@ -21,11 +56,6 @@ type Client struct {
 	version    string
 	httpClient *http.Client
 }
-
-var (
-	httpCache map[string]*http.Client
-	cacheLock sync.Mutex
-)
 
 // VolumeDriver returns a REST wrapper for the VolumeDriver interface.
 func (c *Client) VolumeDriver() volume.VolumeDriver {
@@ -39,9 +69,9 @@ func (c *Client) ClusterManager() cluster.Cluster {
 
 // Status sends a Status request at the /status REST endpoint.
 func (c *Client) Status() (*Status, error) {
-	var status Status
-	err := c.Get().UsePath("/status").Do().Unmarshal(&status)
-	return &status, err
+	status := &Status{}
+	err := c.Get().UsePath("/status").Do().Unmarshal(status)
+	return status, err
 }
 
 // Get returns a Request object setup for GET call.
@@ -79,10 +109,6 @@ func newHTTPClient(u *url.URL, tlsConfig *tls.Config, timeout time.Duration) *ht
 	}
 
 	switch u.Scheme {
-	default:
-		httpTransport.Dial = func(proto, addr string) (net.Conn, error) {
-			return net.DialTimeout(proto, addr, timeout)
-		}
 	case "unix":
 		socketPath := u.Path
 		unixDial := func(proto, addr string) (net.Conn, error) {
@@ -91,33 +117,13 @@ func newHTTPClient(u *url.URL, tlsConfig *tls.Config, timeout time.Duration) *ht
 		}
 		httpTransport.Dial = unixDial
 		unix2HTTP(u)
+	default:
+		httpTransport.Dial = func(proto, addr string) (net.Conn, error) {
+			return net.DialTimeout(proto, addr, timeout)
+		}
 	}
+
 	return &http.Client{Transport: httpTransport}
-}
-
-// NewClient returns a new REST client for specified server.
-func NewClient(host string, version string) (*Client, error) {
-	baseURL, err := url.Parse(host)
-	if err != nil {
-		return nil, err
-	}
-	if baseURL.Path == "" {
-		baseURL.Path = "/"
-	}
-	httpClient := getHttpClient(host)
-	unix2HTTP(baseURL)
-	c := &Client{
-		base:       baseURL,
-		version:    version,
-		httpClient: httpClient,
-	}
-	return c, nil
-}
-
-// NewClusterClient returns a new REST client for cluster management.
-func NewClusterClient() (*Client, error) {
-	sockPath := "unix://" + config.ClusterAPIBase + "osd.sock"
-	return NewClient(sockPath, config.Version)
 }
 
 func getHttpClient(host string) *http.Client {
@@ -129,6 +135,7 @@ func getHttpClient(host string) *http.Client {
 		if !ok {
 			u, err := url.Parse(host)
 			if err != nil {
+				// TODO(pedge): clean up
 				fmt.Println("Failed to parse into url", host)
 				return nil
 			}
@@ -140,14 +147,4 @@ func getHttpClient(host string) *http.Client {
 		}
 	}
 	return c
-}
-
-// NewDriver returns a new REST client for specified driver.
-func NewDriverClient(driverName string) (*Client, error) {
-	sockPath := "unix://" + config.DriverAPIBase + driverName + ".sock"
-	return NewClient(sockPath, config.Version)
-}
-
-func init() {
-	httpCache = make(map[string]*http.Client)
 }
