1626ce0322a7f802831147196f8223d54f03fb4e
Peter Edge <peter.edge@gmail.com>
2016-03-01 03:47:27 +0000

    flexvolume package file
    Signed-off-by: Peter Edge <peter.edge@gmail.com>

---
+++ pkg/flexvolume/flexvolume.go
@@ -0,0 +1,7 @@
+/*
+Package flexvolume implements utility code for Kubernetes flexvolumes.
+
+https://github.com/kubernetes/kubernetes/pull/13840
+https://github.com/kubernetes/kubernetes/tree/master/examples/flexvolume
+*/
+package flexvolume
