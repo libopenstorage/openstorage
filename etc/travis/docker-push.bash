90c584569aa9b99c906ce6baea4f824c1256c553
Peter Edge <peter.edge@gmail.com>
2016-04-19 17:34:46 -0400

    add docker push to travis
    Signed-off-by: Peter Edge <peter.edge@gmail.com>

---
+++ etc/travis/docker-push.bash
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -Ee
+
+DIR="$(cd "$(dirname "$0")/../.." && pwd)"
+cd "${DIR}"
+
+if [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+  echo "TRAVIS_BRANCH is ${TRAVIS_BRANCH}, which is not master, will not docker push openstorage/osd" >&2
+  exit 0
+fi
+
+if [ -z "${DOCKER_EMAIL}" ]; then
+  echo "error: DOCKER_EMAIL not set" >&2
+  exit 1
+fi
+
+if [ -z "${DOCKER_USER}" ]; then
+  echo "error: DOCKER_USER not set" >&2
+  exit 1
+fi
+
+if [ -z "${DOCKER_PASS}" ]; then
+  echo "error: DOCKER_PASS not set" >&2
+  exit 1
+fi
+
+make docker-build-osd
+docker login -e "${DOCKER_EMAIL}" -u "${DOCKER_USER}" -p "${DOCKER_PASS}"
+docker push openstorage/osd
