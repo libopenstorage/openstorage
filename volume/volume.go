bf036475db39ec71b35d7507b690e2d574177058
Peter Edge <peter.edge@gmail.com>
2016-04-25 13:23:00 -0400

    cleanup volume driver
    Signed-off-by: Peter Edge <peter.edge@gmail.com>

--- volume/volume.go
+++ volume/volume.go
@@ -2,6 +2,7 @@ package volume
 
 import (
 	"errors"
+	"fmt"
 	"sync"
 
 	"github.com/libopenstorage/openstorage/api"
@@ -37,22 +38,10 @@ type VolumeDriver interface {
 type IODriver interface {
 	// Read sz bytes from specified volume at specified offset.
 	// Return number of bytes read and error.
-	Read(
-		volumeID string,
-		buf []byte,
-		sz uint64,
-		offset int64,
-	) (int64, error)
-
+	Read(volumeID string, buf []byte, sz uint64, offset int64) (int64, error)
 	// Write sz bytes from specified volume at specified offset.
 	// Return number of bytes written and error.
-	Write(
-		volumeID string,
-		buf []byte,
-		sz uint64,
-		offset int64,
-	) (int64, error)
-
+	Write(volumeID string, buf []byte, sz uint64, offset int64) (int64, error)
 	// Flush writes to stable storage.
 	// Return error.
 	Flush(volumeID string) error
@@ -61,52 +50,36 @@ type IODriver interface {
 // ProtoDriver must be implemented by all volume drivers.  It specifies the
 // most basic functionality, such as creating and deleting volumes.
 type ProtoDriver interface {
-	// String description of this driver.
-	String() string
-
+	fmt.Stringer
 	// Type of this driver
 	Type() api.DriverType
-
 	// Create a new Vol for the specific volume spec.
 	// It returns a system generated VolumeID that uniquely identifies the volume
-	Create(
-		locator *api.VolumeLocator,
-		Source *api.Source,
-		spec *api.VolumeSpec,
-	) (string, error)
-
+	Create(locator *api.VolumeLocator, Source *api.Source, spec *api.VolumeSpec) (string, error)
 	// Delete volume.
 	// Errors ErrEnoEnt, ErrVolHasSnaps may be returned.
 	Delete(volumeID string) error
-
 	// Mount volume at specified path
 	// Errors ErrEnoEnt, ErrVolDetached may be returned.
 	Mount(volumeID string, mountPath string) error
-
 	// Unmount volume at specified path
 	// Errors ErrEnoEnt, ErrVolDetached may be returned.
 	Unmount(volumeID string, mountPath string) error
-
 	// Update not all fields of the spec are supported, ErrNotSupported will be thrown for unsupported
 	// updates.
 	Set(volumeID string, locator *api.VolumeLocator, spec *api.VolumeSpec) error
-
 	// Snapshot create volume snapshot.
 	// Errors ErrEnoEnt may be returned
 	Snapshot(volumeID string, readonly bool, locator *api.VolumeLocator) (string, error)
-
 	// Stats for specified volume.
 	// Errors ErrEnoEnt may be returned
 	Stats(volumeID string) (*api.Stats, error)
-
 	// Alerts on this volume.
 	// Errors ErrEnoEnt may be returned
 	Alerts(volumeID string) (*api.Alerts, error)
-
 	// Status returns a set of key-value pairs which give low
 	// level diagnostic status about this driver.
 	Status() [][2]string
-
 	// Shutdown and cleanup.
 	Shutdown()
 }
@@ -116,11 +89,9 @@ type Enumerator interface {
 	// Inspect specified volumes.
 	// Returns slice of volumes that were found.
 	Inspect(volumeIDs []string) ([]*api.Volume, error)
-
 	// Enumerate volumes that map to the volumeLocator. Locator fields may be regexp.
 	// If locator fields are left blank, this will return all volumes.
 	Enumerate(locator *api.VolumeLocator, labels map[string]string) ([]*api.Volume, error)
-
 	// Enumerate snaps for specified volumes
 	SnapEnumerate(volID []string, snapLabels map[string]string) ([]*api.Volume, error)
 }
@@ -132,7 +103,6 @@ type BlockDriver interface {
 	// On success the devicePath specifies location where the device is exported
 	// Errors ErrEnoEnt, ErrVolAttached may be returned.
 	Attach(volumeID string) (string, error)
-
 	// Detach device from the host.
 	// Errors ErrEnoEnt, ErrVolDetached may be returned.
 	Detach(volumeID string) error
