678dfb141461cd0024e93354b721254450e95d81
Aditya Dani <aditya@portworx.com>
2017-09-06 11:11:22 -0700

    Do not use go.pedge.io for flexvolumes

--- pkg/flexvolume/api_server.go
+++ pkg/flexvolume/api_server.go
@@ -3,48 +3,58 @@ package flexvolume
 import (
 	"time"
 
-	"go.pedge.io/pb/go/google/protobuf"
-	"go.pedge.io/proto/rpclog"
+	"github.com/golang/protobuf/proto"
+	"github.com/golang/protobuf/ptypes/empty"
+	"go.pedge.io/dlog"
 	"golang.org/x/net/context"
 )
 
 type apiServer struct {
-	protorpclog.Logger
 	client Client
 }
 
 func newAPIServer(client Client) *apiServer {
-	return &apiServer{protorpclog.NewLogger("flexvolume.API"), client}
+	return &apiServer{client}
 }
 
-func (a *apiServer) Init(_ context.Context, _ *google_protobuf.Empty) (_ *google_protobuf.Empty, err error) {
-	defer func(start time.Time) { a.Log(nil, nil, err, time.Since(start)) }(time.Now())
+func (a *apiServer) Init(_ context.Context, _ *empty.Empty) (_ *empty.Empty, err error) {
+	defer func(start time.Time) { log("Init", nil, nil, err, time.Since(start)) }(time.Now())
 	return checkClientError(a.client.Init())
 }
 
-func (a *apiServer) Attach(_ context.Context, request *AttachRequest) (_ *google_protobuf.Empty, err error) {
-	defer func(start time.Time) { a.Log(request, nil, err, time.Since(start)) }(time.Now())
+func (a *apiServer) Attach(_ context.Context, request *AttachRequest) (_ *empty.Empty, err error) {
+	defer func(start time.Time) { log("Attach", request, nil, err, time.Since(start)) }(time.Now())
 	return checkClientError(a.client.Attach(request.JsonOptions))
 }
 
-func (a *apiServer) Detach(_ context.Context, request *DetachRequest) (_ *google_protobuf.Empty, err error) {
-	defer func(start time.Time) { a.Log(request, nil, err, time.Since(start)) }(time.Now())
+func (a *apiServer) Detach(_ context.Context, request *DetachRequest) (_ *empty.Empty, err error) {
+	defer func(start time.Time) { log("Detach", request, nil, err, time.Since(start)) }(time.Now())
 	return checkClientError(a.client.Detach(request.MountDevice, false))
 }
 
-func (a *apiServer) Mount(_ context.Context, request *MountRequest) (_ *google_protobuf.Empty, err error) {
-	defer func(start time.Time) { a.Log(request, nil, err, time.Since(start)) }(time.Now())
+func (a *apiServer) Mount(_ context.Context, request *MountRequest) (_ *empty.Empty, err error) {
+	defer func(start time.Time) { log("Mount", request, nil, err, time.Since(start)) }(time.Now())
 	return checkClientError(a.client.Mount(request.TargetMountDir, request.MountDevice, request.JsonOptions))
 }
 
-func (a *apiServer) Unmount(_ context.Context, request *UnmountRequest) (_ *google_protobuf.Empty, err error) {
-	defer func(start time.Time) { a.Log(request, nil, err, time.Since(start)) }(time.Now())
+func (a *apiServer) Unmount(_ context.Context, request *UnmountRequest) (_ *empty.Empty, err error) {
+	defer func(start time.Time) { log("Unmount", request, nil, err, time.Since(start)) }(time.Now())
 	return checkClientError(a.client.Unmount(request.MountDir))
 }
 
-func checkClientError(err error) (*google_protobuf.Empty, error) {
+func checkClientError(err error) (*empty.Empty, error) {
 	if err != nil {
 		return nil, err
 	}
-	return &google_protobuf.Empty{}, nil
+	return &empty.Empty{}, nil
+}
+
+func log(methodName string, request proto.Message, response proto.Message, err error, duration time.Duration) {
+	if err != nil {
+		dlog.Errorf("Method: %v Request: %v Response: %v Error: %v Duration: %v",
+			methodName, request.String(), response.String(), err.Error(), duration.String())
+	} else {
+		dlog.Infof("Method: %v Request: %v Response: %v Error: %v Duration: %v",
+			methodName, request.String(), response.String(), err.Error(), duration.String())
+	}
 }
