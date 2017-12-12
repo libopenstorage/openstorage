4af854db1de8711a79a78f57499b024cda8b5daf
Harsh Desai <harsh@portworx.com>
2017-12-11 16:32:47 -0800

    Add storage operations interface which will be implemented by providers like AWS and GCE

---
+++ pkg/storageops/storageops.go
@@ -0,0 +1,74 @@
+package storageops
+
+const (
+	// SetIdentifierNone is an default identifier to group all disks managed by
+	// storage ops
+	SetIdentifierNone = "None"
+)
+
+// Custom storage operation error codes.
+const (
+	_ = iota + 5000
+	ErrVolDetached
+	ErrVolInval
+	ErrVolAttachedOnRemoteNode
+)
+
+// StorageError error returned for storage operations
+type StorageError struct {
+	// Code is one of storage operation driver error codes.
+	Code int
+	// Msg is human understandable error message.
+	Msg string
+	// Instance provides more information on the error.
+	Instance string
+}
+
+// Ops interface to perform basic storage operations.
+type Ops interface {
+	// Name returns name of the storage operations driver
+	Name() string
+	// Create volume based on input template volume and also apply given labels.
+	Create(template interface{}, labels map[string]string) (interface{}, error)
+	// GetDeviceID returns ID/Name of the given device/disk
+	GetDeviceID(template interface{}) string
+	// Attach volumeID.
+	// Return attach path.
+	Attach(volumeID string) (string, error)
+	// Detach volumeID.
+	Detach(volumeID string) error
+	// Delete volumeID.
+	Delete(volumeID string) error
+	// FreeDevices returns free block devices on the instance
+	FreeDevices(blockDeviceMappings []interface{}, rootDeviceName string) ([]string, error)
+	// Inspect volumes specified by volumeID
+	Inspect(volumeIds []*string) ([]interface{}, error)
+	// DeviceMappings returns map[local_attached_volume_path]->volume ID/NAME
+	DeviceMappings() (map[string]string, error)
+	// Enumerate volumes that match given filters. Organize them into
+	// sets identified by setIdentifier.
+	// labels can be nil, setIdentifier can be empty string.
+	Enumerate(volumeIds []*string,
+		labels map[string]string,
+		setIdentifier string,
+	) (map[string][]interface{}, error)
+	// DevicePath for the given volume i.e path where it's attached
+	DevicePath(volume interface{}) (string, error)
+	// Snapshot the volume with given volumeID
+	Snapshot(volumeID string, readonly bool) (interface{}, error)
+	// ApplyTags will apply given labels/tags on the given volume
+	ApplyTags(v interface{}, labels map[string]string) error
+	// RemoveTags removes labels/tags from the given volume
+	RemoveTags(v interface{}, labels map[string]string) error
+	// Tags will list the existing labels/tags on the given volume
+	Tags(v interface{}) (map[string]string, error)
+}
+
+// NewStorageError creates a new custom storage error instance
+func NewStorageError(code int, msg string, instance string) error {
+	return &StorageError{Code: code, Msg: msg, Instance: instance}
+}
+
+func (e *StorageError) Error() string {
+	return e.Msg
+}
