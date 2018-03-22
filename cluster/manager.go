5d70f4c49be54d79bbc3185b0908b96bea057e2e
Saurabh Deoras <sdeoras@gmail.com>
2018-03-22 12:20:01 -0700

    osdconfig during node decommission (#371)
    osdconfig cleanup during node decommission
    
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>
    
    * osdconfig node remove decommission
    
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>
    
    * osdconfig delete before del from db
    
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- cluster/manager.go
+++ cluster/manager.go
@@ -1509,8 +1509,12 @@ func (c *ClusterManager) NodeRemoveDone(nodeID string, result error) {
 
 	logrus.Infof("Cluster manager node remove done: node ID %s", nodeID)
 
-	err := c.deleteNodeFromDB(nodeID)
-	if err != nil {
+	// Remove osdconfig data from etcd
+	if err := c.configManager.DeleteNodeConf(nodeID); err != nil {
+		logrus.Warn("error removing node from osdconfig:", err)
+	}
+
+	if err := c.deleteNodeFromDB(nodeID); err != nil {
 		msg := fmt.Sprintf("Failed to delete node %s "+
 			"from cluster database, error %s",
 			nodeID, err)
