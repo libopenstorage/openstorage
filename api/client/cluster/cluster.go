6831a291d6d31aea65531a8907ac9e3e6ed036eb
Luis Pabón <lpabon@users.noreply.github.com>
2018-04-18 23:56:52 -0400

    Revert "Reorganize to create cluster.ClusterClient"

--- api/client/cluster/cluster.go
+++ api/client/cluster/cluster.go
@@ -10,15 +10,14 @@ const (
 	OsdSocket = "osd"
 )
 
-// ClusterManager a cluster.ClusterClient implementation
-func ClusterManager(c *client.Client) cluster.ClusterClient {
-	return New(c)
+// ClusterManager returns a REST wrapper for the Cluster interface.
+func ClusterManager(c *client.Client) cluster.Cluster {
+	return newClusterClient(c)
 }
 
 // NewAuthClusterClient returns a new REST client.
-//     host: REST endpoint [http://<ip>:<port> OR unix://<path-to-unix-socket>].
-//     default: [unix://var/lib/osd/cluster/osd.sock]
-//     version: Cluster API version
+// host: REST endpoint [http://<ip>:<port> OR unix://<path-to-unix-socket>]. default: [unix://var/lib/osd/cluster/osd.sock]
+// version: Cluster API version
 func NewAuthClusterClient(host, version string, authstring string, accesstoken string) (*client.Client, error) {
 	if host == "" {
 		host = client.GetUnixServerPath(OsdSocket, cluster.APIBase)
@@ -33,9 +32,8 @@ func NewAuthClusterClient(host, version string, authstring string, accesstoken s
 }
 
 // NewClusterClient returns a new REST client.
-//     host: REST endpoint [http://<ip>:<port> OR unix://<path-to-unix-socket>].
-//     default: [unix://var/lib/osd/cluster/osd.sock]
-//     version: Cluster API version
+// host: REST endpoint [http://<ip>:<port> OR unix://<path-to-unix-socket>]. default: [unix://var/lib/osd/cluster/osd.sock]
+// version: Cluster API version
 func NewClusterClient(host, version string) (*client.Client, error) {
 	if host == "" {
 		host = client.GetUnixServerPath(OsdSocket, cluster.APIBase)
@@ -49,11 +47,8 @@ func NewClusterClient(host, version string) (*client.Client, error) {
 	return client.NewClient(host, version, "")
 }
 
-// GetSupportedClusterVersions returns a list of supported versions of the Cluster API.
-//
-//     host: REST endpoint [http://<ip>:<port> OR unix://<path-to-unix-socket>].
-//     default: [unix://var/lib/osd/cluster/osd.sock]
-//
+// GetSupportedClusterVersions returns a list of supported versions of the Cluster API
+// host: REST endpoint [http://<ip>:<port> OR unix://<path-to-unix-socket>]. default: [unix://var/lib/osd/cluster/osd.sock]
 func GetSupportedClusterVersions(host string) ([]string, error) {
 	if host == "" {
 		host = client.GetUnixServerPath(OsdSocket, cluster.APIBase)
