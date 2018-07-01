8cdd2935fb1cdde639bb12e7d1d400cce7635959
Saurabh Deoras <sdeoras@gmail.com>
2018-07-01 22:09:06 +0000

    generated code from proto
    Signed-off-by: Saurabh Deoras <sdeoras@gmail.com>

--- pkg/flexvolume/flexvolume.pb.gw.go
+++ pkg/flexvolume/flexvolume.pb.gw.go
@@ -104,14 +104,14 @@ func RegisterAPIHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, 
 	defer func() {
 		if err != nil {
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Printf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 			return
 		}
 		go func() {
 			<-ctx.Done()
 			if cerr := conn.Close(); cerr != nil {
-				grpclog.Printf("Failed to close conn to %s: %v", endpoint, cerr)
+				grpclog.Infof("Failed to close conn to %s: %v", endpoint, cerr)
 			}
 		}()
 	}()
@@ -125,8 +125,8 @@ func RegisterAPIHandler(ctx context.Context, mux *runtime.ServeMux, conn *grpc.C
 	return RegisterAPIHandlerClient(ctx, mux, NewAPIClient(conn))
 }
 
-// RegisterAPIHandler registers the http handlers for service API to "mux".
-// The handlers forward requests to the grpc endpoint over the given implementation of "APIClient".
+// RegisterAPIHandlerClient registers the http handlers for service API
+// to "mux". The handlers forward requests to the grpc endpoint over the given implementation of "APIClient".
 // Note: the gRPC framework executes interceptors within the gRPC handler. If the passed in "APIClient"
 // doesn't go through the normal gRPC flow (creating a gRPC client etc.) then it will be up to the passed in
 // "APIClient" to call the correct interceptors.
