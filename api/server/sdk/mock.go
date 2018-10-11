00e9d263ef2f8ee34b300b52da485926c7fd9cb1
Saurabh Deoras <saurabh@portworx.com>
2018-10-11 14:23:06 -0700

    wire gen bug fixes
    Signed-off-by: Saurabh Deoras <saurabh@portworx.com>

--- api/server/sdk/mock.go
+++ api/server/sdk/mock.go
@@ -3,6 +3,10 @@ package sdk
 import (
 	"github.com/golang/mock/gomock"
 	"github.com/kubernetes-csi/csi-test/utils"
+	"github.com/libopenstorage/openstorage/alerts"
+	"github.com/libopenstorage/openstorage/alerts/mock"
+	"github.com/libopenstorage/openstorage/cluster"
+	"github.com/libopenstorage/openstorage/cluster/mock"
 	"github.com/libopenstorage/openstorage/volume"
 	"github.com/libopenstorage/openstorage/volume/drivers"
 	"github.com/libopenstorage/openstorage/volume/drivers/mock"
@@ -12,6 +16,9 @@ const MockDriver = "mock"
 
 // NewMockNet is a provider of mock net.
 func NewMockNet() Net { return Net("tcp") }
+
+// NewMockRestPort is a provider of mock rest port.
+func NewMockRestPort() RestPort { return RestPort("") }
 
 // NewMockAddress is a provider of mock address.
 func NewMockAddress() Address { return Address("127.0.0.1:0") }
@@ -26,6 +33,16 @@ func NewMockDriver(volDriver *mockdriver.MockVolumeDriver) (Driver, error) {
 
 	// Register mock driver and return
 	return Driver(MockDriver), volumedrivers.Register(MockDriver, nil)
+}
+
+// NewMockCluster is a provider of mock cluster.
+func NewMockCluster(cluster *mockcluster.MockCluster) cluster.Cluster {
+	return cluster
+}
+
+// NewMockFilterDeleter is a provider of mock filter deleter.
+func NewMockFilterDeleter(filterDelter *mockalerts.MockFilterDeleter) alerts.FilterDeleter {
+	return filterDelter
 }
 
 // NewMockTestReporter is a provider for TestReporter
