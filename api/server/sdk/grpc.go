54031293672c61afe4f899be102bbda191c0f9fd
Saurabh Deoras <saurabh@portworx.com>
2018-10-11 12:44:17 -0700

    testing wire gen
    Signed-off-by: Saurabh Deoras <saurabh@portworx.com>

--- api/server/sdk/grpc.go
+++ api/server/sdk/grpc.go
@@ -2,11 +2,8 @@ package sdk
 
 import "github.com/libopenstorage/openstorage/pkg/grpcserver"
 
-type GrpcNet string
-type GrpcAddress string
-
 // NewGrpcServer is a provider of GrpcServer
-func NewGrpcServer(net GrpcNet, address GrpcAddress) (*grpcserver.GrpcServer, error) {
+func NewGrpcServer(net NetStr, address AddrStr) (*grpcserver.GrpcServer, error) {
 	return grpcserver.New(&grpcserver.GrpcServerConfig{
 		Name:    "SDK",
 		Net:     string(net),
