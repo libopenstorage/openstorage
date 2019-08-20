68ebce7feb5f3586a98bcd69cda9ecd08693fa53
Grant Griffiths <ggp493@gmail.com>
2019-08-20 15:59:06 -0700

    Add Vault, KVDB, and Docker auth token_secret support (#1196)
    Signed-off-by: Grant Griffiths <grant@portworx.com>

--- pkg/auth/secrets/secrets_test.go
+++ pkg/auth/secrets/secrets_test.go
@@ -4,116 +4,178 @@ import (
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
+	"github.com/libopenstorage/openstorage/api"
 	"github.com/libopenstorage/secrets"
-	"github.com/libopenstorage/secrets/k8s"
 	"github.com/libopenstorage/secrets/mock"
 	"github.com/stretchr/testify/require"
 )
 
-func TestNewAuthOnError(t *testing.T) {
+func TestNewAuth(t *testing.T) {
 	a, err := NewAuth(TypeK8s, nil)
 	require.EqualError(t, ErrSecretsNotInitialized, err.Error(), "Expected an error on NewAuth")
 	require.Nil(t, a, "Expected auth object to be nil")
 
 	secretsInstance, _ := getSecretsMock(t)
-	a, err = NewAuth(5, secretsInstance)
+	a, err = NewAuth(111, secretsInstance)
 	require.Error(t, err, "Expected an error on NewAuth")
 	require.Nil(t, a, "Expected auth object to be nil")
 }
 
-func TestK8sGetToken(t *testing.T) {
-	s, mockSecret := getSecretsMock(t)
-	a, err := NewAuth(TypeK8s, s)
-	require.NoError(t, err, "Expected no error on auth")
-	name := "secret-name"
-	namespace := "ns"
-	token := "auth-token"
-	mockSecret.EXPECT().
-		GetSecret(
-			name,
-			map[string]string{
-				k8s.SecretNamespace: namespace,
-			}).
-		Return(map[string]interface{}{SecretTokenKey: token}, nil).
-		Times(1)
-	gotToken, err := a.GetToken(name, namespace)
-	require.NoError(t, err, "Unexpected error on GetToken")
-	require.Equal(t, gotToken, token, "Unexpected token returned")
-}
+func TestGetToken(t *testing.T) {
+	tt := []struct {
+		testName string
 
-func TestK8sGetTokenWithEmptyTokent(t *testing.T) {
-	s, mockSecret := getSecretsMock(t)
-	a, err := NewAuth(TypeK8s, s)
-	require.NoError(t, err, "Expected no error on auth")
-	name := "secret-name"
-	namespace := "ns"
-	mockSecret.EXPECT().
-		GetSecret(
-			name,
-			map[string]string{
-				k8s.SecretNamespace: namespace,
-			}).
-		Return(map[string]interface{}{}, nil).
-		Times(1)
-	_, err = a.GetToken(name, namespace)
-	require.EqualError(t, ErrAuthTokenNotFound, err.Error(), "Unexpected error on GetToken")
-}
+		secretType      AuthTokenProviders
+		secretName      string
+		secretNamespace string
+		token           string
+		publicData      bool
+		customData      bool
 
-func TestDCOSGetToken(t *testing.T) {
-	s, mockSecret := getSecretsMock(t)
-	a, err := NewAuth(TypeDCOS, s)
-	require.NoError(t, err, "Expected no error on auth")
-	name := "secret-name"
-	namespace := "ns"
-	token := "auth-token"
-	key := namespace + "/" + name
-	mockSecret.EXPECT().
-		GetSecret(
-			key,
-			nil,
-		).
-		Return(map[string]interface{}{key: token}, nil).
-		Times(1)
-	gotToken, err := a.GetToken(name, namespace)
-	require.NoError(t, err, "Unexpected error on GetToken")
-	require.Equal(t, gotToken, token, "Unexpected token returned")
-}
+		expectError   bool
+		expectedError string
+		expectedToken string
+	}{
+		{
+			testName: "k8s get token",
 
-func TestDCOSGetTokenWithNoNamespace(t *testing.T) {
-	s, mockSecret := getSecretsMock(t)
-	a, err := NewAuth(TypeDCOS, s)
-	require.NoError(t, err, "Expected no error on auth")
-	name := "secret-name"
-	token := "auth-token"
-	key := name
-	mockSecret.EXPECT().
-		GetSecret(
-			key,
-			nil,
-		).
-		Return(map[string]interface{}{key: token}, nil).
-		Times(1)
-	gotToken, err := a.GetToken(name, "")
-	require.NoError(t, err, "Unexpected error on GetToken")
-	require.Equal(t, gotToken, token, "Unexpected token returned")
-}
+			secretType:      TypeK8s,
+			secretName:      "secret-name-k8s",
+			secretNamespace: "ns",
+			token:           "auth-token",
 
-func TestDCOSGetTokenWithEmptyToken(t *testing.T) {
-	s, mockSecret := getSecretsMock(t)
-	a, err := NewAuth(TypeDCOS, s)
-	require.NoError(t, err, "Expected no error on auth")
-	name := "secret-name"
-	namespace := "ns"
-	key := namespace + "/" + name
-	mockSecret.EXPECT().
-		GetSecret(
-			key,
-			nil,
-		).
-		Return(map[string]interface{}{}, nil).
-		Times(1)
-	_, err = a.GetToken(name, namespace)
-	require.EqualError(t, ErrAuthTokenNotFound, err.Error(), "Unexpected error on GetToken")
+			expectError: false,
+		},
+		{
+			testName: "k8s empty token",
+
+			secretType:      TypeK8s,
+			secretName:      "secret-name-k8s-empty",
+			secretNamespace: "ns",
+			token:           "",
+
+			expectError:   true,
+			expectedError: ErrAuthTokenNotFound.Error(),
+		},
+		{
+			testName: "dcos get token",
+
+			secretType:      TypeDCOS,
+			secretName:      "secret-name-dcos",
+			secretNamespace: "ns",
+			token:           "auth-token",
+
+			expectError:   false,
+			expectedToken: "ns/secret-name-dcos",
+		},
+		{
+			testName: "dcos empty token",
+
+			secretType:      TypeDCOS,
+			secretName:      "secret-name-dcos-empty",
+			secretNamespace: "ns",
+			token:           "",
+
+			expectError:   true,
+			expectedError: ErrAuthTokenNotFound.Error(),
+		},
+		{
+			testName: "dcos empty namespace",
+
+			secretType:      TypeDCOS,
+			secretName:      "secret-name-dcos-no-ns",
+			secretNamespace: "",
+			token:           "abcd",
+
+			expectError: false,
+		},
+		{
+			testName: "vault get token",
+
+			secretType: TypeVault,
+			secretName: "secret-name-vault",
+			token:      "auth-token",
+
+			expectError: false,
+		},
+		{
+			testName: "docker get token",
+
+			secretType: TypeDocker,
+			secretName: "secret-name-docker",
+			token:      "auth-token",
+
+			expectError: false,
+		},
+		{
+			testName: "kvdb get token",
+
+			secretType: TypeKVDB,
+			secretName: "secret-name-kvdb",
+			token:      "auth-token",
+
+			expectError: false,
+		},
+	}
+
+	for _, tc := range tt {
+		secretContext := make(map[string]string)
+		if tc.secretNamespace != "" {
+			secretContext[SecretNamespaceKey] = tc.secretNamespace
+		}
+		if tc.publicData {
+			secretContext[secrets.PublicSecretData] = "true"
+		}
+		if tc.customData {
+			secretContext[secrets.CustomSecretData] = "true"
+		}
+		s, mockSecret := getSecretsMock(t)
+		a, err := NewAuth(tc.secretType, s)
+		require.NoError(t, err, "Expected no error on auth")
+		expectedTokenResponse := make(map[string]interface{})
+		if !tc.expectError {
+			switch tc.secretType {
+			case TypeDCOS:
+				key := tc.secretName
+				if tc.secretNamespace != "" {
+					key = tc.secretNamespace + "/" + key
+				}
+				expectedTokenResponse[key] = tc.expectedToken
+			case TypeK8s:
+				expectedTokenResponse[SecretTokenKey] = tc.expectedToken
+			default:
+				expectedTokenResponse[SecretNameKey] = tc.expectedToken
+			}
+		}
+		mockSecret.EXPECT().
+			GetSecret(
+				gomock.Any(),
+				gomock.Any(),
+			).
+			Return(expectedTokenResponse, nil).
+			Times(1)
+
+		req := &api.TokenSecretContext{
+			SecretName:      tc.secretName,
+			SecretNamespace: tc.secretNamespace,
+		}
+		gotToken, err := a.GetToken(req)
+		if tc.expectError {
+			if err == nil {
+				t.Errorf("[%s]: Expected error on GetToken, but got nil", tc.testName)
+			}
+			if err != nil && err.Error() != tc.expectedError {
+				t.Errorf("[%s]: Expected error '%s' on GetToken, but got '%s'", tc.testName, err.Error(), tc.expectedError)
+			}
+		} else {
+			if err != nil {
+				t.Errorf("[%s]: Expected no error on GetToken, but got '%s'", tc.testName, err.Error())
+			}
+			if gotToken != tc.expectedToken {
+				t.Errorf("[%s]: Expected token '%s' on GetToken, but got '%s'", tc.testName, tc.expectedToken, gotToken)
+			}
+		}
+	}
 }
 
 func getSecretsMock(t *testing.T) (secrets.Secrets, *mock.MockSecrets) {
