63ac38a0dae3c3574c8c239a7cab5c5700acda04
Grant Griffiths <ggp493@gmail.com>
2019-09-13 13:29:52 -0700

    CSI: Add req.Secrets to SDK calls for volume encryption (#1226)
    - req.Secrets encryption info added to locator.VolumeLabels on CreateVolume
    - req.Secrets encryption info added to req.DriverOptions on NodePublishVolume
    - req.Secrets has auth-token stripped so it is not stored in Labels
    
    Signed-off-by: Grant Griffiths <grant@portworx.com>

--- csi/csi_test.go
+++ csi/csi_test.go
@@ -43,6 +43,7 @@ import (
 	"github.com/libopenstorage/openstorage/config"
 	"github.com/libopenstorage/openstorage/pkg/auth"
 	"github.com/libopenstorage/openstorage/pkg/grpcserver"
+	"github.com/libopenstorage/openstorage/pkg/options"
 	"github.com/libopenstorage/openstorage/pkg/role"
 	"github.com/libopenstorage/openstorage/pkg/storagepolicy"
 	"github.com/libopenstorage/openstorage/volume"
@@ -381,4 +382,22 @@ func TestNewCSIServerBadParameters(t *testing.T) {
 	assert.NotNil(t, err)
 	assert.Contains(t, err.Error(), "Unable to setup server")
 	os.Remove(tester.uds)
+}
+
+func TestAddEncryptionInfoToLabels(t *testing.T) {
+	s := OsdCsiServer{}
+
+	secrets := map[string]string{
+		options.OptionsSecret:        "secret",
+		options.OptionsSecretContext: "context",
+		options.OptionsSecretKey:     "key",
+	}
+	labels := map[string]string{
+		"test": "val",
+	}
+	labels = s.addEncryptionInfoToLabels(labels, secrets)
+
+	assert.Equal(t, labels[options.OptionsSecret], "secret")
+	assert.Equal(t, labels[options.OptionsSecretContext], "context")
+	assert.Equal(t, labels[options.OptionsSecretKey], "key")
 }
