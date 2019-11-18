4c03e1f47e03fcf1b3e97814094c0c2449eddb76
vjs-px <56370204+vjs-px@users.noreply.github.com>
2019-11-18 10:24:33 -0800

    Support for Background Volume Services (#1324)
    This Change adds interface changes to support scheduling of
    background service tasks on volumes. The current proposal is to be able to
    schedule fstrim and fsck on volumes
    
    api/api.proto, api/api.go, api/api.pb.go : Contains the structure definitions and
    other contants
    
    volume/volume.go : contains new service API added to protoDriver Interface defintion
    
    api/client/volume/client.go : Rest Client method to support volume service API
    api/server/volume.go : ReST Server handler for volume service API
    
    volume/drivers/mock/driver.mock.go : Auto generated for docker tests
    
    other files: Dummy implementation for the new service API
    
    Signed-off-by: vjs-px <vjs@portworx.com>

--- volume/drivers/fake/fake.go
+++ volume/drivers/fake/fake.go
@@ -953,3 +953,7 @@ func (d *driver) Catalog(volumeID, path, depth string) (api.CatalogResponse, err
 		},
 	}, nil
 }
+
+func (d *driver) VolService(volumeID string, vtreq *api.VolumeServiceRequest) (*api.VolumeServiceResponse, error) {
+	return nil, volume.ErrNotSupported
+}
