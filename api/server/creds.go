850f709c0f34bb283339ddd39e4693aaeb97bf52
stgleb <glebstepanov1992@gmail.com>
2019-12-18 12:31:33 +0200

    Revert "Use sdk for creds endpoints"
    This reverts commit 33063b62deba3efd50bd78d245d260a600ae909b.
    
    Signed-off-by: stgleb <glebstepanov1992@gmail.com>

--- api/server/creds.go
+++ api/server/creds.go
@@ -3,7 +3,6 @@ package server
 import (
 	"encoding/json"
 	"net/http"
-	"strconv"
 
 	"github.com/gorilla/mux"
 	"github.com/libopenstorage/openstorage/api"
@@ -12,148 +11,58 @@ import (
 func (vd *volAPI) credsEnumerate(w http.ResponseWriter, r *http.Request) {
 	method := "credsEnumerate"
 
-	ctx, err := vd.annotateContext(r)
+	d, err := vd.getVolDriver(r)
+	if err != nil {
+		notFound(w, r)
+		return
+	}
 
+	creds, err := d.CredsEnumerate()
 	if err != nil {
 		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	// Get gRPC connection
-	conn, err := vd.getConn()
-	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	client := api.NewOpenStorageCredentialsClient(conn)
-	resp, err := client.Enumerate(ctx, &api.SdkCredentialEnumerateRequest{})
-
-	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	credentials := make(map[string]interface{})
-
-	for _, credentialId := range resp.CredentialIds {
-		credResp, err := client.Inspect(ctx, &api.SdkCredentialInspectRequest{
-			CredentialId: credentialId,
-		})
-
-		if err != nil {
-			vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		credentials[credResp.CredentialId] = credResp
-	}
-
-	if err := json.NewEncoder(w).Encode(credentials); err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+	json.NewEncoder(w).Encode(creds)
 }
 
 func (vd *volAPI) credsCreate(w http.ResponseWriter, r *http.Request) {
 	method := "credsCreate"
-	var (
-		err error
-	)
+	var input api.CredCreateRequest
+	response := &api.CredCreateResponse{}
 
-	input := api.CredCreateRequest{
-		InputParams: make(map[string]string),
-	}
-
-	if err = json.NewDecoder(r.Body).Decode(&input.InputParams); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
 		vd.sendError(vd.name, method, w, err.Error(), http.StatusBadRequest)
 		return
 	}
-
-	useProxy := false
-
-	if len(input.InputParams[api.OptCredProxy]) != 0 {
-		useProxy, err = strconv.ParseBool(input.InputParams[api.OptCredProxy])
-
-		if err != nil {
-			vd.sendError(vd.name, method, w, err.Error(), http.StatusBadRequest)
-			return
-		}
-	}
-
-	sdkCreds, err := api.GetSdkCreds(input.InputParams)
-
+	d, err := vd.getVolDriver(r)
 	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusBadRequest)
+		notFound(w, r)
 		return
 	}
 
-	sdkReq := &api.SdkCredentialCreateRequest{
-		Name:           input.InputParams[api.OptName],
-		Bucket:         input.InputParams[api.OptCredBucket],
-		EncryptionKey:  input.InputParams[api.OptCredEncrKey],
-		UseProxy:       useProxy,
-		CredentialType: sdkCreds,
-	}
-	ctx, err := vd.annotateContext(r)
-
+	response.UUID, err = d.CredsCreate(input.InputParams)
 	if err != nil {
 		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-
-	// Get gRPC connection
-	conn, err := vd.getConn()
-	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	client := api.NewOpenStorageCredentialsClient(conn)
-	resp, err := client.Create(ctx, sdkReq)
-	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	if err := json.NewEncoder(w).Encode(&api.CredCreateResponse{
-		UUID: resp.CredentialId,
-	}); err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusBadRequest)
-		return
-	}
+	json.NewEncoder(w).Encode(response)
 }
 
 func (vd *volAPI) credsDelete(w http.ResponseWriter, r *http.Request) {
 	method := "credsDelete"
 	vars := mux.Vars(r)
 	uuid, ok := vars["uuid"]
-
 	if !ok {
 		vd.sendError(vd.name, method, w, "Could not parse form for uuid", http.StatusBadRequest)
 		return
 	}
 
-	// Initialize volume driver with connection
-	ctx, err := vd.annotateContext(r)
-
+	d, err := vd.getVolDriver(r)
 	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
+		notFound(w, r)
 		return
 	}
-
-	// Get gRPC connection
-	conn, err := vd.getConn()
-	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	client := api.NewOpenStorageCredentialsClient(conn)
-
-	_, err = client.Delete(ctx, &api.SdkCredentialDeleteRequest{
-		CredentialId: uuid,
-	})
-
-	if err != nil {
+	if err = d.CredsDelete(uuid); err != nil {
 		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
 		return
 	}
@@ -168,28 +77,13 @@ func (vd *volAPI) credsValidate(w http.ResponseWriter, r *http.Request) {
 		vd.sendError(vd.name, method, w, "Could not parse form for uuid", http.StatusBadRequest)
 		return
 	}
-	// Initialize volume driver with connection
-	ctx, err := vd.annotateContext(r)
-
+	d, err := vd.getVolDriver(r)
 	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
+		notFound(w, r)
 		return
 	}
 
-	// Get gRPC connection
-	conn, err := vd.getConn()
-	if err != nil {
-		vd.sendError(vd.name, method, w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-	client := api.NewOpenStorageCredentialsClient(conn)
-
-	_, err = client.Validate(ctx, &api.SdkCredentialValidateRequest{
-		CredentialId: uuid,
-	})
-
-	if err != nil {
+	if err := d.CredsValidate(uuid); err != nil {
 		vd.sendError(vd.name, method, w, err.Error(), http.StatusUnprocessableEntity)
 		return
 	}
