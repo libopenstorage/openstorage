a02c66c872645e74a0eff13d699e19be455483bd
Grant Griffiths <ggp493@gmail.com>
2020-02-05 16:24:20 -0800

    Change setup CSI OSD cluster to use DaemonSet instead of binary (#1418)
    Signed-off-by: Grant Griffiths <grant@portworx.com>

--- hack/setup-kind-csi-cluster.sh
+++ hack/setup-kind-csi-cluster.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
+KIND_IMAGE=kindest/node:v1.17.0
 set -x
 cd $GOPATH/src/github.com/libopenstorage/openstorage
+
+# Setup KinD cluster
 if [ $DELETE_CLUSTER ]; then
 	kind delete cluster --name kind-csi 
 fi
-kind create cluster --name kind-csi --config hack/kind.yaml --image kindest/node:v1.17.0
-docker cp $GOPATH/bin/osd kind-csi-worker:/osdkind
-docker exec -ti kind-csi-worker bash -c "chmod 777 /osdkind"
-docker exec -ti kind-csi-worker bash -c "mkdir -p /var/lib/kubelet/plugins/osd.openstorage.org"
-docker exec -ti kind-csi-worker bash -c "pkill osdkind"
-docker exec -tid kind-csi-worker bash -c "CSI_ENDPOINT=/var/lib/kubelet/plugins/osd.openstorage.org/csi.sock /osdkind -d --driver=name=fake --sdkport 9106 --sdkrestport 9116 --csidrivername osd.openstorage.org"
+kind create cluster --name kind-csi --config hack/kind.yaml --image $KIND_IMAGE
+
+# Build OSD
+make docker-build-osd
+
+# Load local OSD image into KinD
+kind load docker-image quay.io/openstorage/osd:latest --name kind-csi
 export KUBECONFIG=$(kind get kubeconfig-path --name kind-csi)
+
+# Start OSD
+kubectl delete -f hack/osd-csi.yaml
 kubectl apply -f hack/osd-csi.yaml
 
 set +x
