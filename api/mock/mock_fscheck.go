e3b7d9d62cb3db54cd65dc756aa41be338957cff
prabirpaul <prabir@portworx.com>
2020-04-07 19:32:21 -0700

    Implementation specific volumes attributes support (#1496)
    * Implementation specific volumes attributes support
    
    Signed-off-by: prabir <prabir@portworx.com>

--- api/mock/mock_fscheck.go
+++ api/mock/mock_fscheck.go
@@ -12,30 +12,30 @@ import (
 	reflect "reflect"
 )
 
-// MockOpenStorageFilesystemCheckServer is a mock of OpenStorageFilesystemCheckServer interface
+// MockOpenStorageFilesystemCheckServer is a mock of OpenStorageFilesystemCheckServer interface.
 type MockOpenStorageFilesystemCheckServer struct {
 	ctrl     *gomock.Controller
 	recorder *MockOpenStorageFilesystemCheckServerMockRecorder
 }
 
-// MockOpenStorageFilesystemCheckServerMockRecorder is the mock recorder for MockOpenStorageFilesystemCheckServer
+// MockOpenStorageFilesystemCheckServerMockRecorder is the mock recorder for MockOpenStorageFilesystemCheckServer.
 type MockOpenStorageFilesystemCheckServerMockRecorder struct {
 	mock *MockOpenStorageFilesystemCheckServer
 }
 
-// NewMockOpenStorageFilesystemCheckServer creates a new mock instance
+// NewMockOpenStorageFilesystemCheckServer creates a new mock instance.
 func NewMockOpenStorageFilesystemCheckServer(ctrl *gomock.Controller) *MockOpenStorageFilesystemCheckServer {
 	mock := &MockOpenStorageFilesystemCheckServer{ctrl: ctrl}
 	mock.recorder = &MockOpenStorageFilesystemCheckServerMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockOpenStorageFilesystemCheckServer) EXPECT() *MockOpenStorageFilesystemCheckServerMockRecorder {
 	return m.recorder
 }
 
-// CheckHealth mocks base method
+// CheckHealth mocks base method.
 func (m *MockOpenStorageFilesystemCheckServer) CheckHealth(arg0 context.Context, arg1 *api.SdkFilesystemCheckCheckHealthRequest) (*api.SdkFilesystemCheckCheckHealthResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CheckHealth", arg0, arg1)
@@ -44,13 +44,13 @@ func (m *MockOpenStorageFilesystemCheckServer) CheckHealth(arg0 context.Context,
 	return ret0, ret1
 }
 
-// CheckHealth indicates an expected call of CheckHealth
+// CheckHealth indicates an expected call of CheckHealth.
 func (mr *MockOpenStorageFilesystemCheckServerMockRecorder) CheckHealth(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CheckHealth", reflect.TypeOf((*MockOpenStorageFilesystemCheckServer)(nil).CheckHealth), arg0, arg1)
 }
 
-// CheckHealthGetStatus mocks base method
+// CheckHealthGetStatus mocks base method.
 func (m *MockOpenStorageFilesystemCheckServer) CheckHealthGetStatus(arg0 context.Context, arg1 *api.SdkFilesystemCheckCheckHealthGetStatusRequest) (*api.SdkFilesystemCheckCheckHealthGetStatusResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "CheckHealthGetStatus", arg0, arg1)
@@ -59,13 +59,13 @@ func (m *MockOpenStorageFilesystemCheckServer) CheckHealthGetStatus(arg0 context
 	return ret0, ret1
 }
 
-// CheckHealthGetStatus indicates an expected call of CheckHealthGetStatus
+// CheckHealthGetStatus indicates an expected call of CheckHealthGetStatus.
 func (mr *MockOpenStorageFilesystemCheckServerMockRecorder) CheckHealthGetStatus(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CheckHealthGetStatus", reflect.TypeOf((*MockOpenStorageFilesystemCheckServer)(nil).CheckHealthGetStatus), arg0, arg1)
 }
 
-// FixAll mocks base method
+// FixAll mocks base method.
 func (m *MockOpenStorageFilesystemCheckServer) FixAll(arg0 context.Context, arg1 *api.SdkFilesystemCheckFixAllRequest) (*api.SdkFilesystemCheckFixAllResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "FixAll", arg0, arg1)
@@ -74,13 +74,13 @@ func (m *MockOpenStorageFilesystemCheckServer) FixAll(arg0 context.Context, arg1
 	return ret0, ret1
 }
 
-// FixAll indicates an expected call of FixAll
+// FixAll indicates an expected call of FixAll.
 func (mr *MockOpenStorageFilesystemCheckServerMockRecorder) FixAll(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FixAll", reflect.TypeOf((*MockOpenStorageFilesystemCheckServer)(nil).FixAll), arg0, arg1)
 }
 
-// FixAllGetStatus mocks base method
+// FixAllGetStatus mocks base method.
 func (m *MockOpenStorageFilesystemCheckServer) FixAllGetStatus(arg0 context.Context, arg1 *api.SdkFilesystemCheckFixAllGetStatusRequest) (*api.SdkFilesystemCheckFixAllGetStatusResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "FixAllGetStatus", arg0, arg1)
@@ -89,13 +89,13 @@ func (m *MockOpenStorageFilesystemCheckServer) FixAllGetStatus(arg0 context.Cont
 	return ret0, ret1
 }
 
-// FixAllGetStatus indicates an expected call of FixAllGetStatus
+// FixAllGetStatus indicates an expected call of FixAllGetStatus.
 func (mr *MockOpenStorageFilesystemCheckServerMockRecorder) FixAllGetStatus(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FixAllGetStatus", reflect.TypeOf((*MockOpenStorageFilesystemCheckServer)(nil).FixAllGetStatus), arg0, arg1)
 }
 
-// Stop mocks base method
+// Stop mocks base method.
 func (m *MockOpenStorageFilesystemCheckServer) Stop(arg0 context.Context, arg1 *api.SdkFilesystemCheckStopRequest) (*api.SdkFilesystemCheckStopResponse, error) {
 	m.ctrl.T.Helper()
 	ret := m.ctrl.Call(m, "Stop", arg0, arg1)
@@ -104,36 +104,36 @@ func (m *MockOpenStorageFilesystemCheckServer) Stop(arg0 context.Context, arg1 *
 	return ret0, ret1
 }
 
-// Stop indicates an expected call of Stop
+// Stop indicates an expected call of Stop.
 func (mr *MockOpenStorageFilesystemCheckServerMockRecorder) Stop(arg0, arg1 interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Stop", reflect.TypeOf((*MockOpenStorageFilesystemCheckServer)(nil).Stop), arg0, arg1)
 }
 
-// MockOpenStorageFilesystemCheckClient is a mock of OpenStorageFilesystemCheckClient interface
+// MockOpenStorageFilesystemCheckClient is a mock of OpenStorageFilesystemCheckClient interface.
 type MockOpenStorageFilesystemCheckClient struct {
 	ctrl     *gomock.Controller
 	recorder *MockOpenStorageFilesystemCheckClientMockRecorder
 }
 
-// MockOpenStorageFilesystemCheckClientMockRecorder is the mock recorder for MockOpenStorageFilesystemCheckClient
+// MockOpenStorageFilesystemCheckClientMockRecorder is the mock recorder for MockOpenStorageFilesystemCheckClient.
 type MockOpenStorageFilesystemCheckClientMockRecorder struct {
 	mock *MockOpenStorageFilesystemCheckClient
 }
 
-// NewMockOpenStorageFilesystemCheckClient creates a new mock instance
+// NewMockOpenStorageFilesystemCheckClient creates a new mock instance.
 func NewMockOpenStorageFilesystemCheckClient(ctrl *gomock.Controller) *MockOpenStorageFilesystemCheckClient {
 	mock := &MockOpenStorageFilesystemCheckClient{ctrl: ctrl}
 	mock.recorder = &MockOpenStorageFilesystemCheckClientMockRecorder{mock}
 	return mock
 }
 
-// EXPECT returns an object that allows the caller to indicate expected use
+// EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockOpenStorageFilesystemCheckClient) EXPECT() *MockOpenStorageFilesystemCheckClientMockRecorder {
 	return m.recorder
 }
 
-// CheckHealth mocks base method
+// CheckHealth mocks base method.
 func (m *MockOpenStorageFilesystemCheckClient) CheckHealth(arg0 context.Context, arg1 *api.SdkFilesystemCheckCheckHealthRequest, arg2 ...grpc.CallOption) (*api.SdkFilesystemCheckCheckHealthResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0, arg1}
@@ -146,14 +146,14 @@ func (m *MockOpenStorageFilesystemCheckClient) CheckHealth(arg0 context.Context,
 	return ret0, ret1
 }
 
-// CheckHealth indicates an expected call of CheckHealth
+// CheckHealth indicates an expected call of CheckHealth.
 func (mr *MockOpenStorageFilesystemCheckClientMockRecorder) CheckHealth(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CheckHealth", reflect.TypeOf((*MockOpenStorageFilesystemCheckClient)(nil).CheckHealth), varargs...)
 }
 
-// CheckHealthGetStatus mocks base method
+// CheckHealthGetStatus mocks base method.
 func (m *MockOpenStorageFilesystemCheckClient) CheckHealthGetStatus(arg0 context.Context, arg1 *api.SdkFilesystemCheckCheckHealthGetStatusRequest, arg2 ...grpc.CallOption) (*api.SdkFilesystemCheckCheckHealthGetStatusResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0, arg1}
@@ -166,14 +166,14 @@ func (m *MockOpenStorageFilesystemCheckClient) CheckHealthGetStatus(arg0 context
 	return ret0, ret1
 }
 
-// CheckHealthGetStatus indicates an expected call of CheckHealthGetStatus
+// CheckHealthGetStatus indicates an expected call of CheckHealthGetStatus.
 func (mr *MockOpenStorageFilesystemCheckClientMockRecorder) CheckHealthGetStatus(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "CheckHealthGetStatus", reflect.TypeOf((*MockOpenStorageFilesystemCheckClient)(nil).CheckHealthGetStatus), varargs...)
 }
 
-// FixAll mocks base method
+// FixAll mocks base method.
 func (m *MockOpenStorageFilesystemCheckClient) FixAll(arg0 context.Context, arg1 *api.SdkFilesystemCheckFixAllRequest, arg2 ...grpc.CallOption) (*api.SdkFilesystemCheckFixAllResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0, arg1}
@@ -186,14 +186,14 @@ func (m *MockOpenStorageFilesystemCheckClient) FixAll(arg0 context.Context, arg1
 	return ret0, ret1
 }
 
-// FixAll indicates an expected call of FixAll
+// FixAll indicates an expected call of FixAll.
 func (mr *MockOpenStorageFilesystemCheckClientMockRecorder) FixAll(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FixAll", reflect.TypeOf((*MockOpenStorageFilesystemCheckClient)(nil).FixAll), varargs...)
 }
 
-// FixAllGetStatus mocks base method
+// FixAllGetStatus mocks base method.
 func (m *MockOpenStorageFilesystemCheckClient) FixAllGetStatus(arg0 context.Context, arg1 *api.SdkFilesystemCheckFixAllGetStatusRequest, arg2 ...grpc.CallOption) (*api.SdkFilesystemCheckFixAllGetStatusResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0, arg1}
@@ -206,14 +206,14 @@ func (m *MockOpenStorageFilesystemCheckClient) FixAllGetStatus(arg0 context.Cont
 	return ret0, ret1
 }
 
-// FixAllGetStatus indicates an expected call of FixAllGetStatus
+// FixAllGetStatus indicates an expected call of FixAllGetStatus.
 func (mr *MockOpenStorageFilesystemCheckClientMockRecorder) FixAllGetStatus(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FixAllGetStatus", reflect.TypeOf((*MockOpenStorageFilesystemCheckClient)(nil).FixAllGetStatus), varargs...)
 }
 
-// Stop mocks base method
+// Stop mocks base method.
 func (m *MockOpenStorageFilesystemCheckClient) Stop(arg0 context.Context, arg1 *api.SdkFilesystemCheckStopRequest, arg2 ...grpc.CallOption) (*api.SdkFilesystemCheckStopResponse, error) {
 	m.ctrl.T.Helper()
 	varargs := []interface{}{arg0, arg1}
@@ -226,7 +226,7 @@ func (m *MockOpenStorageFilesystemCheckClient) Stop(arg0 context.Context, arg1 *
 	return ret0, ret1
 }
 
-// Stop indicates an expected call of Stop
+// Stop indicates an expected call of Stop.
 func (mr *MockOpenStorageFilesystemCheckClientMockRecorder) Stop(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
