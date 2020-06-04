54ba9b79fcb89c1919716ecbbfd23cd614e2e553
Grant Griffiths <grant@portworx.com>
2020-06-03 22:22:33 -0700

    Add fix for consistent mount table read (#1593)
    Signed-off-by: Grant Griffiths <grant@portworx.com>

--- pkg/mount/nfs.go
+++ pkg/mount/nfs.go
@@ -104,7 +104,7 @@ func (m *nfsMounter) normalizeSource(info *mount.Info, host string) {
 // Load mount table
 func (m *nfsMounter) Load(source []string) error {
 	m.traceCache = append(m.traceCache, "Entered nfsMounter.Load()")
-	info, err := mount.GetMounts()
+	info, err := GetMounts()
 	if err != nil {
 		return err
 	}
