64522ffea9aa3a15f1040606ba35d6b0c8071a22
Grant Griffiths <grant@portworx.com>
2020-07-22 15:57:22 -0700

    fix test.sh
    Signed-off-by: Grant Griffiths <grant@portworx.com>

--- demo/test.sh
+++ demo/test.sh
@@ -5,14 +5,19 @@ fail() {
   exit 1
 }
 
+# pre-generated token added to secret
+token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAb3BlbnN0b3JhZ2UuaW8iLCJleHAiOjE3NTMxNDA0NDcsImdyb3VwcyI6WyIqIl0sImlhdCI6MTU5NTQ2MDQ0NywiaXNzIjoib3BlbnN0b3JhZ2UuaW8iLCJuYW1lIjoidXNlciIsInJvbGVzIjpbInN5c3RlbS51c2VyIl0sInN1YiI6InVzZXJAb3BlbnN0b3JhZ2UuaW8ifQ.41yebvGhSUlks4_perFh0sORmGnpulEML-7plFa0WWE
+kubectl create secret generic token-secret --from-literal=auth-token=$token
+kubectl get secret token-secret || fail "failed to create token secrets"
+
 # Create Storage Class
-kubectl apply -f e2e/storageclass.yaml || fail "failed to apply storageclass yaml"
+kubectl apply -f demo/e2e/storageclass.yaml || fail "failed to apply storageclass yaml"
 kubectl get storageclass openstorage-sc || fail "failed to create storageclass"
 
 # Create PVC
-kubectl apply -f e2e/pvc.yaml || fail "failed to apply PVC yaml"
+kubectl apply -f demo/e2e/pvc.yaml || fail "failed to apply PVC yaml"
 kubectl get pvc openstorage-pvc || fail "failed to create pvc"
 
 # Use PVC with MySQL deployment
-kubectl apply -f e2e/mysql.yaml || fail "failed to apply deployment yaml"
+kubectl apply -f demo/e2e/mysql.yaml || fail "failed to apply deployment yaml"
 kubectl get deployment mysql || fail "failed to create deployment"
