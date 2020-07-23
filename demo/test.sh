fa97b34972d5068beff46d795191232fda7e0c14
Sarah Khalife <51349105+skhalife@users.noreply.github.com>
2020-07-23 11:37:25 -0700

    Updated testing

--- demo/test.sh
+++ demo/test.sh
@@ -25,5 +25,3 @@ kubectl get pvc openstorage-pvc | grep Bound || exitFail "PVC not bound after 15
 
 # Use PVC with MySQL deployment
 kubectl apply -f demo/e2e/mysql.yaml || exitFail "failed to apply deployment yaml"
-sleep 15
-kubectl get deployment mysql || exitFail "failed to create deployment"
