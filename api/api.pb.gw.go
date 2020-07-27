65209ee2be846f166eaa2de743af0382dbcc9d42
vjs-px <56370204+vjs-px@users.noreply.github.com>
2020-07-27 10:46:56 -0700

    Filesystem trim: renamed GetStatus() api to Status() (#1691)
    * Filesystem trim: renamed GetStatus() api to Status()
    https://portworx.atlassian.net/browse/PWX-14716
    
    Signed-off-by: vjs-px <vjs@portworx.com>
    
    * changed version and rebuild binaries
    
    Signed-off-by: vjs-px <vjs@portworx.com>

--- api/api.pb.gw.go
+++ api/api.pb.gw.go
@@ -165,18 +165,18 @@ func request_OpenStorageFilesystemTrim_Start_0(ctx context.Context, marshaler ru
 }
 
 var (
-	filter_OpenStorageFilesystemTrim_GetStatus_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
+	filter_OpenStorageFilesystemTrim_Status_0 = &utilities.DoubleArray{Encoding: map[string]int{}, Base: []int(nil), Check: []int(nil)}
 )
 
-func request_OpenStorageFilesystemTrim_GetStatus_0(ctx context.Context, marshaler runtime.Marshaler, client OpenStorageFilesystemTrimClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
-	var protoReq SdkFilesystemTrimGetStatusRequest
+func request_OpenStorageFilesystemTrim_Status_0(ctx context.Context, marshaler runtime.Marshaler, client OpenStorageFilesystemTrimClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq SdkFilesystemTrimStatusRequest
 	var metadata runtime.ServerMetadata
 
-	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_OpenStorageFilesystemTrim_GetStatus_0); err != nil {
+	if err := runtime.PopulateQueryParameters(&protoReq, req.URL.Query(), filter_OpenStorageFilesystemTrim_Status_0); err != nil {
 		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
 	}
 
-	msg, err := client.GetStatus(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	msg, err := client.Status(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
 	return msg, metadata, err
 
 }
@@ -2123,7 +2123,7 @@ func RegisterOpenStorageFilesystemTrimHandlerClient(ctx context.Context, mux *ru
 
 	})
 
-	mux.Handle("GET", pattern_OpenStorageFilesystemTrim_GetStatus_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("GET", pattern_OpenStorageFilesystemTrim_Status_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		if cn, ok := w.(http.CloseNotifier); ok {
@@ -2141,14 +2141,14 @@ func RegisterOpenStorageFilesystemTrimHandlerClient(ctx context.Context, mux *ru
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
-		resp, md, err := request_OpenStorageFilesystemTrim_GetStatus_0(rctx, inboundMarshaler, client, req, pathParams)
+		resp, md, err := request_OpenStorageFilesystemTrim_Status_0(rctx, inboundMarshaler, client, req, pathParams)
 		ctx = runtime.NewServerMetadataContext(ctx, md)
 		if err != nil {
 			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
 			return
 		}
 
-		forward_OpenStorageFilesystemTrim_GetStatus_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+		forward_OpenStorageFilesystemTrim_Status_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -2187,7 +2187,7 @@ func RegisterOpenStorageFilesystemTrimHandlerClient(ctx context.Context, mux *ru
 var (
 	pattern_OpenStorageFilesystemTrim_Start_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "filesystem-trim", "start"}, ""))
 
-	pattern_OpenStorageFilesystemTrim_GetStatus_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "filesystem-trim", "status"}, ""))
+	pattern_OpenStorageFilesystemTrim_Status_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "filesystem-trim", "status"}, ""))
 
 	pattern_OpenStorageFilesystemTrim_Stop_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "filesystem-trim", "stop"}, ""))
 )
@@ -2195,7 +2195,7 @@ var (
 var (
 	forward_OpenStorageFilesystemTrim_Start_0 = runtime.ForwardResponseMessage
 
-	forward_OpenStorageFilesystemTrim_GetStatus_0 = runtime.ForwardResponseMessage
+	forward_OpenStorageFilesystemTrim_Status_0 = runtime.ForwardResponseMessage
 
 	forward_OpenStorageFilesystemTrim_Stop_0 = runtime.ForwardResponseMessage
 )
