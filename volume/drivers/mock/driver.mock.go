a873a7d2a84316c7098b78c08f4555b20a29c088
vjs-px <56370204+vjs-px@users.noreply.github.com>
2020-07-27 10:46:56 -0700

    Filesystem trim: renamed GetStatus() api to Status() (#1691)
    * Filesystem trim: renamed GetStatus() api to Status()
    https://portworx.atlassian.net/browse/PWX-14716
    
    Signed-off-by: vjs-px <vjs@portworx.com>
    
    * changed version and rebuild binaries
    
    Signed-off-by: vjs-px <vjs@portworx.com>

--- volume/drivers/mock/driver.mock.go
+++ volume/drivers/mock/driver.mock.go
@@ -392,17 +392,6 @@ func (_mr *_MockVolumeDriverRecorder) FilesystemCheckStop(arg0 interface{}) *gom
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "FilesystemCheckStop", arg0)
 }
 
-func (_m *MockVolumeDriver) FilesystemTrimGetStatus(_param0 *api.SdkFilesystemTrimGetStatusRequest) (*api.SdkFilesystemTrimGetStatusResponse, error) {
-	ret := _m.ctrl.Call(_m, "FilesystemTrimGetStatus", _param0)
-	ret0, _ := ret[0].(*api.SdkFilesystemTrimGetStatusResponse)
-	ret1, _ := ret[1].(error)
-	return ret0, ret1
-}
-
-func (_mr *_MockVolumeDriverRecorder) FilesystemTrimGetStatus(arg0 interface{}) *gomock.Call {
-	return _mr.mock.ctrl.RecordCall(_mr.mock, "FilesystemTrimGetStatus", arg0)
-}
-
 func (_m *MockVolumeDriver) FilesystemTrimStart(_param0 *api.SdkFilesystemTrimStartRequest) (*api.SdkFilesystemTrimStartResponse, error) {
 	ret := _m.ctrl.Call(_m, "FilesystemTrimStart", _param0)
 	ret0, _ := ret[0].(*api.SdkFilesystemTrimStartResponse)
@@ -412,6 +401,17 @@ func (_m *MockVolumeDriver) FilesystemTrimStart(_param0 *api.SdkFilesystemTrimSt
 
 func (_mr *_MockVolumeDriverRecorder) FilesystemTrimStart(arg0 interface{}) *gomock.Call {
 	return _mr.mock.ctrl.RecordCall(_mr.mock, "FilesystemTrimStart", arg0)
+}
+
+func (_m *MockVolumeDriver) FilesystemTrimStatus(_param0 *api.SdkFilesystemTrimStatusRequest) (*api.SdkFilesystemTrimStatusResponse, error) {
+	ret := _m.ctrl.Call(_m, "FilesystemTrimStatus", _param0)
+	ret0, _ := ret[0].(*api.SdkFilesystemTrimStatusResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+func (_mr *_MockVolumeDriverRecorder) FilesystemTrimStatus(arg0 interface{}) *gomock.Call {
+	return _mr.mock.ctrl.RecordCall(_mr.mock, "FilesystemTrimStatus", arg0)
 }
 
 func (_m *MockVolumeDriver) FilesystemTrimStop(_param0 *api.SdkFilesystemTrimStopRequest) (*api.SdkFilesystemTrimStopResponse, error) {
