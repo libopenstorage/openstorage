c4056a24964611b04a8f2961a66a78517e29fa58
Veda Talakad <talakad@users.noreply.github.com>
2020-09-03 16:57:27 -0700

    Allow credential names/uuids with '/' (#1744)
    * Allow credential names with '/'
    
    Signed-off-by: veda <veda@portworx.com>
    
    * Fix go fmt.
    
    Signed-off-by: veda <veda@portworx.com>

--- api/client/volume/client.go
+++ api/client/volume/client.go
@@ -7,6 +7,7 @@ import (
 	"io"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strconv"
 
 	"github.com/libopenstorage/openstorage/api"
@@ -509,7 +510,7 @@ func (v *volumeClient) CredsDelete(uuid string) error {
 // CredsValidate validates the credential by accessuing the cloud
 // provider with the given credential
 func (v *volumeClient) CredsValidate(uuid string) error {
-	req := v.c.Put().Resource(api.OsdCredsPath + "/validate").Instance(uuid)
+	req := v.c.Put().Resource(api.OsdCredsPath + "/validate").Instance(url.QueryEscape(uuid))
 	response := req.Do()
 	if response.Error() != nil {
 		if response.StatusCode() == http.StatusUnprocessableEntity {
