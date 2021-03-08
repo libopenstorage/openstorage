1ee2e4938fb0af77506354b15099f63ec3948d10
Aditya Dani <aditya@portworx.com>
2021-03-07 16:50:49 -0800

    Add support for cluster pairing over https endpoints.
    - This ensures that two clusters can pair over an https endpoint
      with TLS config set to skip verify if the clusters are not TLS enabled.
    
    Signed-off-by: Aditya Dani <aditya@portworx.com>

--- api/client/cluster/cluster.go
+++ api/client/cluster/cluster.go
@@ -1,6 +1,9 @@
 package cluster
 
 import (
+	"crypto/tls"
+	"net/url"
+
 	"github.com/libopenstorage/openstorage/api/client"
 	"github.com/libopenstorage/openstorage/cluster"
 )
@@ -30,6 +33,46 @@ func NewAuthClusterClient(host, version string, authstring string, accesstoken s
 	}
 
 	return client.NewAuthClient(host, version, authstring, accesstoken, "")
+}
+
+// NewInsecureTLSAuthClusterClient returns a new REST client that will skip TLS verification for https
+// host: REST endpoint [http(s)://<ip>:<port>]
+// version: ClusterAPI version
+func NewInsecureTLSAuthClusterClient(host, version, auth string, accesstoken string) (*client.Client, error) {
+	if host == "" {
+		host = client.GetUnixServerPath(OsdSocket, cluster.APIBase)
+	}
+
+	if version == "" {
+		// Set the default version
+		version = cluster.APIVersion
+	}
+
+	var (
+		skipTLSVerify bool
+	)
+	u, err := url.Parse(host)
+	if err == nil && len(u.Scheme) != 0 {
+		if u.Scheme == "https" {
+			// We don't support cert validation yet
+			skipTLSVerify = true
+		} else if u.Scheme != "http" {
+			// In certain cases like AWS ELB - ae20db68c7cb34616b16837ab395fe9c-1428320453.us-east-2.elb.amazonaws.com
+			// url.Parse returns scheme as the actual endpoint
+			host = "http://" + host
+		} // else u.Scheme == http
+	} else {
+		host = "http://" + host
+	}
+
+	clnt, err := client.NewAuthClient(host, version, auth, accesstoken, "")
+	if err != nil {
+		return nil, err
+	}
+	if skipTLSVerify {
+		clnt.SetTLS(&tls.Config{InsecureSkipVerify: true})
+	}
+	return clnt, nil
 }
 
 // NewClusterClient returns a new REST client.
