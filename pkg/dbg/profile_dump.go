134fd8ef7aa73aa42430d52f6dad497b47f6f7d3
jrivera-px <jose@portworx.com>
2021-03-23 17:01:18 -0700

    PWX-18523: Change profile stack & heap names to include the hostname … (#1823)
    * PWX-18523: Change stack & heap filename to include the hostname and 'YYYYMMDDHHMMSS'.
    
    Signed-off-by: Jose Rivera <jose@portworx.com>
    
    * PWX-18523: Export functions use to create profile name.
    
    Signed-off-by: Jose Rivera <jose@portworx.com>

--- pkg/dbg/profile_dump.go
+++ pkg/dbg/profile_dump.go
@@ -16,6 +16,20 @@ const (
 	fnameFmt = "2006-01-02T15:04:05.999999-0700MST"
 )
 
+// GetTimeStamp returns 'readable' timestamp with no spaces 'YYYYMMDDHHMMSS'
+func GetTimeStamp() string {
+	tnow := time.Now()
+	return fmt.Sprintf("%d%02d%02d%02d%02d%02d",
+		tnow.Year(), tnow.Month(), tnow.Day(),
+		tnow.Hour(), tnow.Minute(), tnow.Second())
+}
+
+// GetHostNamePrefix return '<hostname>' to use as prefix.
+func GetHostNamePrefix() string {
+	hname, _ := os.Hostname()
+	return hname
+}
+
 // DumpGoMemoryTrace output memory profile to logs.
 func DumpGoMemoryTrace() {
 	m := &runtime.MemStats{}
@@ -29,11 +43,11 @@ func DumpGoMemoryTrace() {
 func DumpGoProfile() error {
 	trace := make([]byte, 5120*1024)
 	len := runtime.Stack(trace, true)
-	return ioutil.WriteFile(path+time.Now().Format(fnameFmt)+".stack", trace[:len], 0644)
+	return ioutil.WriteFile(path+GetHostNamePrefix()+"-"+GetTimeStamp()+".stack", trace[:len], 0644)
 }
 
 func DumpHeap() {
-	f, err := os.Create(path + time.Now().Format(fnameFmt) + ".heap")
+	f, err := os.Create(path + GetHostNamePrefix() + "-" + GetTimeStamp() + ".heap")
 	if err != nil {
 		logrus.Errorf("could not create memory profile: %v", err)
 		return
