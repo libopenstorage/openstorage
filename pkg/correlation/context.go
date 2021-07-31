c48f68b8bb32bf64c9a2ec1b39381c74e1ac09fc
Grant Griffiths <grant@portworx.com>
2021-07-30 21:04:23 -0700

    Add correlation context implementation
    Signed-off-by: Grant Griffiths <grant@portworx.com>

---
+++ pkg/correlation/context.go
@@ -0,0 +1,36 @@
+package correlation
+
+import (
+	"context"
+
+	"github.com/pborman/uuid"
+)
+
+const (
+	// ContextKey represents the key for storing and retrieving
+	// the correlation context in a context.Context object.
+	ContextKey = "correlation-context"
+)
+
+// RequestContext represents the context for a given a request.
+// A request represents a single action received from an SDK
+// user, container orchestrator, or any other request.
+type RequestContext struct {
+	// ID is a randomly generated UUID per requst
+	ID string
+
+	// Origin is the starting point for this request.
+	// Examples may include any of the following:
+	// pxctl, pxc, kubernetes, CSI, SDK, etc
+	Origin string
+}
+
+// NewContext returns a new correlation context object
+func NewContext(ctx context.Context, origin correlation.Component) context.Context {
+	requestContext := &RequestContext{
+		ID:     uuid.New(),
+		Origin: origin,
+	}
+
+	return context.WithValue(ctx, ContextKey, requestContext)
+}
