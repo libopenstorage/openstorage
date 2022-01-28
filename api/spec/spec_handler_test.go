34dc6b80cc679914a391a2864493950a454cb91e
Veda Talakad <talakad@users.noreply.github.com>
2022-01-27 16:57:55 -0800

    Autofstrim's default value in volume spec (#1978)
    When explicitly not specified, autofstrim to inherit its value from
    nodiscard filed on volume spec.
    
    Signed-off-by: veda <veda@portworx.com>

--- api/spec/spec_handler_test.go
+++ api/spec/spec_handler_test.go
@@ -503,4 +503,10 @@ func TestOptAutoFstrim(t *testing.T) {
 
 	spec = testSpecFromString(t, api.SpecAutoFstrim, "false")
 	require.False(t, spec.AutoFstrim, "Failed to parse auto_fstrim option into spec")
+	// with Nodiscard, when autoFstrim is not specified
+	spec = testSpecFromString(t, api.SpecNodiscard, "true")
+	require.Equal(t, spec.AutoFstrim, true, "AutoFstrim value does not match nodiscard")
+
+	spec = testSpecFromString(t, api.SpecNodiscard, "false")
+	require.Equal(t, spec.AutoFstrim, false, "AutoFstrim value does not match Nodiscard")
 }
