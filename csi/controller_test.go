587434f701b094d55bb0445fcc06dd1f4c24abb4
Grant Griffiths <ggp493@gmail.com>
2023-02-17 09:25:11 -0800

    Revert "CSI: Cleanup failed snapshot and re-create on next retry (#2213)"
    This reverts commit 1affc178d23d6961d4f196898acb0ad87cb67be1.

--- csi/controller_test.go
+++ csi/controller_test.go
@@ -2862,7 +2862,6 @@ func TestControllerCreateSnapshotIdempotent(t *testing.T) {
 		Spec: &api.VolumeSpec{
 			Size: uint64(size),
 		},
-		Status: api.VolumeStatus_VOLUME_STATUS_UP,
 	}
 
 	// Snapshot already exists
