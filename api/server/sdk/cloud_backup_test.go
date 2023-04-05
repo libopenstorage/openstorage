567daa8240006effad6cff6f94d7d718f7a62b6f
Tengxiao Wang <twang@purestorage.com>
2023-04-05 22:26:42 +0000

    Added verification for new fields in TestSdkCloudBackupSize
    Signed-off-by: Tengxiao Wang <twang@purestorage.com>

--- api/server/sdk/cloud_backup_test.go
+++ api/server/sdk/cloud_backup_test.go
@@ -6,7 +6,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -1147,9 +1147,14 @@ func TestSdkCloudBackupSize(t *testing.T) {
 
 	id := "pxbackup"
 	creds := "creds"
-	backupSize := uint64(176433)
+	totalDownloadSize := uint64(1764330)
+	compressedSize := uint64(214535)
+	capacityForRestore := uint64(523046)
 	resp := &api.SdkCloudBackupSizeResponse{
-		Size: backupSize,
+		Size: totalDownloadSize,
+		TotalDownloadBytes: totalDownloadSize,
+		CompressedObjectBytes: compressedSize,
+		CapacityRequiredForRestore: capacityForRestore,
 	}
 
 	// Create response
@@ -1166,5 +1171,8 @@ func TestSdkCloudBackupSize(t *testing.T) {
 	r, err := c.Size(context.Background(), &api.SdkCloudBackupSizeRequest{BackupId: id, CredentialId: creds})
 	assert.NoError(t, err)
 	assert.NotNil(t, r.GetSize())
-	assert.Equal(t, r.GetSize(), backupSize)
+	assert.Equal(t, r.GetSize(), totalDownloadSize)
+	assert.Equal(t, r.GetTotalDownloadBytes(), totalDownloadSize)
+	assert.Equal(t, r.GetCompressedObjectBytes(), compressedSize)
+	assert.Equal(t, r.GetCapacityRequiredForRestore(), capacityForRestore)
 }
