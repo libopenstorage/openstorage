2c5c9e9927063282ff804e3d4f891e0dd138c523
Divyam Goel <dgoel@purestorage.com>
2023-04-25 22:28:27 +0000

    Revert "Remove cred check for nearsync"
    This reverts commit 2380ecc165144c509e88c06cd013b602be4f5779.
    
    Signed-off-by: Divyam Goel <dgoel@purestorage.com>

--- api/server/sdk/cloud_backup.go
+++ api/server/sdk/cloud_backup.go
@@ -50,7 +50,7 @@ func (s *CloudBackupServer) Create(
 	if len(req.GetVolumeId()) == 0 {
 		return nil, status.Error(codes.InvalidArgument, "Must supply a volume id")
 	}
-	if !req.NearSyncMigrate && len(req.GetCredentialId()) == 0 {
+	if len(req.GetCredentialId()) == 0 {
 		credId, err = s.defaultCloudBackupCreds(ctx)
 		if err != nil {
 			return nil, err
