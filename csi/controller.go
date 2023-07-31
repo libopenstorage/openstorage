ce913f13b389d32e2fe8069b42d478ccfe547376
Amol Shinde <ashinde@purestorage.com>
2023-07-31 09:18:06 -0700

    PWX-23536: fix to block the rawblock request for FBDA.
    Signed-off-by: Amol Shinde <ashinde@purestorage.com>

--- csi/controller.go
+++ csi/controller.go
@@ -432,12 +432,6 @@ func validateCreateVolumeCapabilitiesPure(caps []*csi.VolumeCapability, proxySpe
 				"FlashArray Direct Access shared filesystems are not supported",
 			)
 		}
-		if block {
-			return status.Errorf(
-				codes.InvalidArgument,
-				"FlashArray Direct Access raw block devices are not yet supported",
-			)
-		}
 	}
 
 	// Check for FB DA volumes: all allowed except raw block
