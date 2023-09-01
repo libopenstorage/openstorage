e831d90e6bb49135c89ea7841659e2eccfb39ac7
Tengxiao Wang <twang@purestorage.com>
2023-09-01 20:59:40 +0000

    Change CreateRebalanceSchedule API from PUT to POST
    Signed-off-by: Tengxiao Wang <twang@purestorage.com>

--- api/api.pb.gw.go
+++ api/api.pb.gw.go
@@ -5806,7 +5806,7 @@ func RegisterOpenStoragePoolHandlerServer(ctx context.Context, mux *runtime.Serv
 
 	})
 
-	mux.Handle("PUT", pattern_OpenStoragePool_CreateRebalanceSchedule_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_OpenStoragePool_CreateRebalanceSchedule_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		var stream runtime.ServerTransportStream
@@ -9060,7 +9060,7 @@ func RegisterOpenStoragePoolHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
-	mux.Handle("PUT", pattern_OpenStoragePool_CreateRebalanceSchedule_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+	mux.Handle("POST", pattern_OpenStoragePool_CreateRebalanceSchedule_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
 		ctx, cancel := context.WithCancel(req.Context())
 		defer cancel()
 		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
