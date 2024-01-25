5295fe25d1b2084e695763fec0e5452e95046483
alice-px <alyu@purestorage.com>
2024-01-25 12:49:34 -0800

    PWX-35577
    Signed-off-by: alice lyu <alyu@purestorage.com>

--- volume/drivers/test/driver.go
+++ volume/drivers/test/driver.go
@@ -321,9 +321,7 @@ func snap(t *testing.T, ctx *Context) {
 	attach(t, ctx)
 	labels := map[string]string{"oh": "snap"}
 	require.NotEqual(t, ctx.volID, "", "invalid volume ID")
-	id, err := ctx.Snapshot(ctx.volID, false,
-		&api.VolumeLocator{Name: "snappy", VolumeLabels: labels},
-		false)
+	id, err := ctx.Snapshot(context.TODO(), ctx.volID, false, &api.VolumeLocator{Name: "snappy", VolumeLabels: labels}, false)
 	require.NoError(t, err, "Failed in creating a snapshot")
 	ctx.snapID = id
 }
