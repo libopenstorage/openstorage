033aaf116fec4084f4cf4b2dac43a1430b16bc47
Piyush Nimbalkar <pnimbalkar@purestorage.com>
2024-04-08 17:37:42 +0000

    PWX-36511 Expose API to return a set of nodes that can be upgraded
    in parallel. This takes an input list of nodes from which the server will return
    a set that doesn't have any common volume within them. This ensures that if
    these nodes are upgraded at once, then a volume with 1+ replica has at least one
    replica somewhere which will be up.
    
    Signed-off-by: Piyush Nimbalkar <pnimbalkar@purestorage.com>

--- api/api.pb.gw.go
+++ api/api.pb.gw.go
@@ -902,6 +902,19 @@ func request_OpenStorageNode_VolumeBytesUsedByNode_0(ctx context.Context, marsha
 
 }
 
+func request_OpenStorageNode_FilterNonOverlappingNodes_0(ctx context.Context, marshaler runtime.Marshaler, client OpenStorageNodeClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq SdkFilterNonOverlappingNodesRequest
+	var metadata runtime.ServerMetadata
+
+	if err := marshaler.NewDecoder(req.Body).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.FilterNonOverlappingNodes(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
 func request_OpenStorageBucket_Create_0(ctx context.Context, marshaler runtime.Marshaler, client OpenStorageBucketClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
 	var protoReq BucketCreateRequest
 	var metadata runtime.ServerMetadata
@@ -4278,6 +4291,35 @@ func RegisterOpenStorageNodeHandlerClient(ctx context.Context, mux *runtime.Serv
 
 	})
 
+	mux.Handle("POST", pattern_OpenStorageNode_FilterNonOverlappingNodes_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		if cn, ok := w.(http.CloseNotifier); ok {
+			go func(done <-chan struct{}, closed <-chan bool) {
+				select {
+				case <-done:
+				case <-closed:
+					cancel()
+				}
+			}(ctx.Done(), cn.CloseNotify())
+		}
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		rctx, err := runtime.AnnotateContext(ctx, mux, req)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_OpenStorageNode_FilterNonOverlappingNodes_0(rctx, inboundMarshaler, client, req, pathParams)
+		ctx = runtime.NewServerMetadataContext(ctx, md)
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_OpenStorageNode_FilterNonOverlappingNodes_0(ctx, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -4299,6 +4341,8 @@ var (
 	pattern_OpenStorageNode_UncordonAttachments_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"v1", "nodes", "attachments", "enable", "node_id"}, ""))
 
 	pattern_OpenStorageNode_VolumeBytesUsedByNode_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "nodes", "bytesused"}, ""))
+
+	pattern_OpenStorageNode_FilterNonOverlappingNodes_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"v1", "nodes", "filter-nonoverlapping"}, ""))
 )
 
 var (
@@ -4319,6 +4363,8 @@ var (
 	forward_OpenStorageNode_UncordonAttachments_0 = runtime.ForwardResponseMessage
 
 	forward_OpenStorageNode_VolumeBytesUsedByNode_0 = runtime.ForwardResponseMessage
+
+	forward_OpenStorageNode_FilterNonOverlappingNodes_0 = runtime.ForwardResponseMessage
 )
 
 // RegisterOpenStorageBucketHandlerFromEndpoint is same as RegisterOpenStorageBucketHandler but
--- api/mock/mock_node.go
+++ api/mock/mock_node.go
@@ -95,6 +95,21 @@ func (mr *MockOpenStorageNodeServerMockRecorder) EnumerateWithFilters(arg0, arg1
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnumerateWithFilters", reflect.TypeOf((*MockOpenStorageNodeServer)(nil).EnumerateWithFilters), arg0, arg1)
 }
 
+// FilterNonOverlappingNodes mocks base method
+func (m *MockOpenStorageNodeServer) FilterNonOverlappingNodes(arg0 context.Context, arg1 *api.SdkFilterNonOverlappingNodesRequest) (*api.SdkFilterNonOverlappingNodesResponse, error) {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "FilterNonOverlappingNodes", arg0, arg1)
+	ret0, _ := ret[0].(*api.SdkFilterNonOverlappingNodesResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// FilterNonOverlappingNodes indicates an expected call of FilterNonOverlappingNodes
+func (mr *MockOpenStorageNodeServerMockRecorder) FilterNonOverlappingNodes(arg0, arg1 interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FilterNonOverlappingNodes", reflect.TypeOf((*MockOpenStorageNodeServer)(nil).FilterNonOverlappingNodes), arg0, arg1)
+}
+
 // Inspect mocks base method
 func (m *MockOpenStorageNodeServer) Inspect(arg0 context.Context, arg1 *api.SdkNodeInspectRequest) (*api.SdkNodeInspectResponse, error) {
 	m.ctrl.T.Helper()
@@ -271,6 +286,26 @@ func (mr *MockOpenStorageNodeClientMockRecorder) EnumerateWithFilters(arg0, arg1
 	mr.mock.ctrl.T.Helper()
 	varargs := append([]interface{}{arg0, arg1}, arg2...)
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "EnumerateWithFilters", reflect.TypeOf((*MockOpenStorageNodeClient)(nil).EnumerateWithFilters), varargs...)
+}
+
+// FilterNonOverlappingNodes mocks base method
+func (m *MockOpenStorageNodeClient) FilterNonOverlappingNodes(arg0 context.Context, arg1 *api.SdkFilterNonOverlappingNodesRequest, arg2 ...grpc.CallOption) (*api.SdkFilterNonOverlappingNodesResponse, error) {
+	m.ctrl.T.Helper()
+	varargs := []interface{}{arg0, arg1}
+	for _, a := range arg2 {
+		varargs = append(varargs, a)
+	}
+	ret := m.ctrl.Call(m, "FilterNonOverlappingNodes", varargs...)
+	ret0, _ := ret[0].(*api.SdkFilterNonOverlappingNodesResponse)
+	ret1, _ := ret[1].(error)
+	return ret0, ret1
+}
+
+// FilterNonOverlappingNodes indicates an expected call of FilterNonOverlappingNodes
+func (mr *MockOpenStorageNodeClientMockRecorder) FilterNonOverlappingNodes(arg0, arg1 interface{}, arg2 ...interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	varargs := append([]interface{}{arg0, arg1}, arg2...)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "FilterNonOverlappingNodes", reflect.TypeOf((*MockOpenStorageNodeClient)(nil).FilterNonOverlappingNodes), varargs...)
 }
 
 // Inspect mocks base method
